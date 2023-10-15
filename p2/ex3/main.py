48e6668fa5116c8d481419d4bb96ef72db8a9ddd
henriquecfonseca <henriquecfonseca21@ua.pt>
2023-10-15 19:28:44 +0100

    Changes to be committed: 	new file:   p2/ex1/ex1.py 	new file:   p2/ex2/main.py 	new file:   p2/ex2/my_function.py 	new file:   p2/ex3/main.py 	new file:   p2/ex3/my_function.py 	new file:   p2/ex4/ex4a.py 	new file:   p2/ex4/ex4c.py 	new file:   p2/ex5/ex5.py

---
+++ p2/ex3/main.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+#shebang line to inform the OS that the content is in python
+
+
+import argparse
+
+# Use imports here
+from colorama import Fore, Back, Style
+from my_function import isPerfect
+
+# Define functions here ...
+
+# maximum_number = 30
+
+
+def main():
+
+    # Process command line arguments
+    parser = argparse.ArgumentParser(description='Script to compute perfect numbers.')
+    parser.add_argument('-mn', '--maximum_number', type=int, help='max number.', required=True)
+    parser.add_argument('-n', '--name', type=str, help='A name to print.', required=False, default='Henrique')
+    parser.add_argument('-sl', '--say_hello', help='Say hello?', action='store_true')
+
+    args = vars(parser.parse_args()) # creates a dictionary
+    print(args)
+
+
+    
+    if args['say_hello']:
+        print("Hi " + args['name'] + " !!!")
+
+
+
+    print("Starting to compute perfect numbers up to " + str(args['maximum_number']))
+    for i in range(1, args['maximum_number']+1):
+        if isPerfect(i):
+            print('Number ' + str(i) + ' is perfect.')
+
+        # dividers = getDividers(i)
+        # print('Number ' + str(i) + ' has dividers ' +str(dividers))
+
+
+if __name__ == "__main__":
+    main()
---
+++ p2/ex4/ex4c.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+#shebang line to inform the OS that the content is in python
+
+
+import argparse
+
+import readchar
+
+# Use imports here
+from colorama import Fore, Back, Style
+
+# Define functions here ...
+
+# maximum_number = 30
+
+def  countNumbersUpto(stop_char):
+
+    print('Start typing')
+
+    keys =[]
+    while True:
+        key = readchar.readkey()
+        keys.append(key)
+        print('You typed ' + key)
+
+        if key == stop_char:
+            break
+
+    print(keys)
+
+    n_numeric = 0
+    for key in keys:
+        if key.isnumeric():
+            n_numeric +=1
+        
+    print('You pressed on ' + str(n_numeric) + ' numeric keys')
+
+def main():
+    countNumbersUpto('x')
+
+
+if __name__ == "__main__":
+    main()
